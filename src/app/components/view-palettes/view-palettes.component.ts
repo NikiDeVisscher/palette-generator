3e2d6f2c403df7a7abfe34f81e1598067424d4be
NikiDeVisscher <niki.de.visscher@gmail.com>
2024-05-14 21:38:00 +0200

    Added edit

--- src/app/components/view-palettes/view-palettes.component.ts
+++ src/app/components/view-palettes/view-palettes.component.ts
@@ -6,17 +6,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { PaletteService } from '../../services/palette.service';
 import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-view-palettes',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule, FormsModule],
   templateUrl: './view-palettes.component.html',
   styleUrl: './view-palettes.component.css'
 })
 export class ViewPalettesComponent implements OnInit {
   palettes: Palette[] = new Array;
   isVisible: boolean = false;
+  editing: string = "";
+  newName: string = "";
 
   constructor(private paletteService: PaletteService, private router: Router) {}
 
@@ -42,6 +45,8 @@ export class ViewPalettesComponent implements OnInit {
   }
 
   onView(index: string | undefined): void {
+    if (this.editing)
+      return;
     this.isVisible = true;
     index = index? index : "0";
     this.router.navigate(['view/' + index]);
@@ -49,7 +54,25 @@ export class ViewPalettesComponent implements OnInit {
 
   onEdit(index: string | undefined, event: Event): void {
     event.stopPropagation();
+    if (this.editing)
+      return;
+
     index = index? index : "0";
+    this.editing = index;
+  }
+
+  onSave(index: number, event: Event): void {
+    event.stopPropagation();
+
+    this.palettes[index].name = this.newName;
+    this.newName = "";
+    this.editing = "";
+
+    this.paletteService.updatePalette(this.palettes[index]).subscribe(
+      () => {
+        this.router.navigate(['view']);
+      }
+    );
   }
 
   onDelete(index: string | undefined, event: Event): void {
